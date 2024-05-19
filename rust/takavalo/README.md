87e3d28e15944945506803658f8089a929a08bd9
Mikko Viitamäki <mikko.viitamaki@logisnext.eu>
2024-05-19 17:35:38 +0300

    poors man version of the cycling light

--- rust/takavalo/README.md
+++ rust/takavalo/README.md
@@ -1,11 +1,7 @@
-# embassy-template
+# Usage
 
-Simple template to generate a embassy project for a few common boards. Make sure you've run `cargo install cargo-generate` before using.
-
-## Usage
-
-```
-cargo generate --git https://github.com/lulf/embassy-template.git
+```rust
+cargo build --release
 ```
 
 Then follow the instructions.
--- rust/takavalo/src/main.rs
+++ rust/takavalo/src/main.rs
@@ -1,27 +1,30 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
+use embassy_time::Timer;
+use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
+
+use embassy_stm32::exti::ExtiInput;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    
-    info!("Hello World!");
-
+    // Configure PA0 as input with pull-down
+    let button = Input::new(p.PA0, Pull::Down);
+    // Configure EXTI (External Interrupt) on PA0
+    let mut exti = ExtiInput::new(button,p.EXTI0);
     let mut led = Output::new(p.PB9, Level::High, Speed::Low);
 
-    loop {
-        info!("high");
-        led.set_high();
-        Timer::after_millis(300).await;
 
-        info!("low");
-        led.set_low();
-        Timer::after_millis(300).await;
+    
+    // Blinking 30s and then try to sleep and waiting for interrupt
+    loop {
+        for _ in 0..100{
+            Timer::after_millis(300).await;
+            led.toggle();
+        }
+        exti.wait_for_rising_edge().await;
     }
 }
